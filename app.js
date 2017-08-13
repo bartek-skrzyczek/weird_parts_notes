afefe8659d1a5b6d645a40472e23bf11f352cad6
bartekbart <bartlomiej@assembla.com>
2017-08-13 18:51:10 +0200

    adding files, sample notes and a file

---
+++ app.js
@@ -0,0 +1,21 @@
+function b() {
+  var myVar;
+  console.log(myVar);
+}
+
+function a() {
+  var myVar = 2;
+  b();
+  console.log(myVar);
+}
+
+var myVar = 1;
+console.log(myVar);
+a();
+console.log(myVar);
+
+1
+undefined
+var myVar = 2;
+b();
+console.log(myVar);
